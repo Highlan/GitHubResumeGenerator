852a1fe57e87ba00814a8f60555501cd658ed926
Fatemeh <fatemeh.gharri@yahoo.com>
2020-06-06 20:09:47 +0200

    Add sructure of repository

--- src/AppBundle/GitHub/Repository.php
+++ src/AppBundle/GitHub/Repository.php
@@ -1,15 +1,74 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Fatemeh
- * Date: 6/6/2020
- * Time: 7:59 PM
- */
 
 namespace AppBundle\GitHub;
 
 
 class Repository
 {
+    private $name;
+    private $language;
+    private $link;
+    private $description;
+    private $watchers;
+    private $forks;
+
+    public function __construct($name, $language, $link, $description, $watchers, $forks)
+    {
+        $this->name = $name;
+        $this->language = $language;
+        $this->link = $link;
+        $this->description = $description;
+        $this->watchers = $watchers;
+        $this->forks = $forks;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string | null
+     */
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLink(): string
+    {
+        return $this->link;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWatchers(): int
+    {
+        return $this->watchers;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getForks(): int
+    {
+        return $this->forks;
+    }
+
 
 }
