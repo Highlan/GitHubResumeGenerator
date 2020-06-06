f883a3f78f1e2d4ad5c694bb7428535a34b86797
Fatemeh <fatemeh.gharri@yahoo.com>
2020-06-06 19:28:23 +0200

    Render user form

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\UserFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,25 @@ class MainController extends AbstractController
      */
     public function indexAction(Request $request)
     {
+        $form = $this->createForm(UserFormType::class);
+        $form->handleRequest($request);
 
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $user = $form->getData();
+
+            return $this->redirectToRoute('resume', ['username' => $user['username']]);
+        }
+        return $this->render('/main/index_form.html.twig', [
+            'userForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("resume", name="resume")
+     */
+    public function resumeAction()
+    {
+        echo 'resume';exit;
     }
 }
