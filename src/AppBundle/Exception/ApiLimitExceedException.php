2fa6dd7b780663fa629c65095c728fe4392bc3fa
Fatemeh <fatemeh.gharri@yahoo.com>
2020-06-06 20:54:46 +0200

    Add some exceptions class

---
+++ src/AppBundle/Exception/ApiLimitExceedException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AppBundle\Exception;
+
+
+class ApiLimitExceedException extends \Exception implements PublishedMessageException
+{
+
+}
---
+++ src/AppBundle/Exception/PublishedMessageException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AppBundle\Exception;
+
+
+interface PublishedMessageException
+{
+    public function getMessage();
+}
