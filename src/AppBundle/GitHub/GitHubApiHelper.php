633fd1ad75fb41cd03f51e54aba9ecbdbaa85b6a
Fatemeh <fatemeh.gharri@yahoo.com>
2020-06-06 19:46:05 +0200

    Prepare structure to send request

--- src/AppBundle/GitHub/GitHubApiHelper.php
+++ src/AppBundle/GitHub/GitHubApiHelper.php
@@ -1,15 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Fatemeh
- * Date: 6/6/2020
- * Time: 7:36 PM
- */
 
 namespace AppBundle\GitHub;
 
 
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\Response;
+
 class GitHubApiHelper
 {
+    const BASE_URL = 'https://api.github.com/';
+    const TOKEN = 'ef6e665f82731a97a3f226c73e1f54c82b72ec60';
+
+
+    private $httpClient;
+
+    public function __construct()
+    {
+        $this->httpClient = HttpClient::create(['headers' => [
+            'Authorization'=> 'token ' . self::TOKEN
+        ]]);
+    }
+
+    public function getUser()
+    {
+
+    }
+
+    private function request(string $method, string $url,  array $option = [])
+    {
+        $response = $this->httpClient->request($method, self::BASE_URL . $url, $option);
+
+        if ($response->getStatusCode() !== Response::HTTP_OK)
+        {
+            dump($response->getStatusCode() ); exit;
+        }
+
+        return $response->toArray();
+    }
 
 }
